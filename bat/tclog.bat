d577713a36693c5d0f32819d8edc8b0b3b6ebdd6
RJK-Engineering <rjklinkhamer@gmail.com>
2019-04-30 19:04:29 +0200

    Renamed tcmd to tc

--- bat/tclog.bat
+++ bat/tclog.bat
@@ -3,7 +3,7 @@
 call "%~dp0..\config.bat"
 
 perl "%rjk-utils-home%\totalcmd\log.pl" ^
---archive-dir "%tcmd-log-archive-dir%" ^
---logfile "%tcmd-log-file%" %*
+--archive-dir "%tc-log-archive-dir%" ^
+--logfile "%tc-log-file%" %*
 
 if %errorlevel% gtr 0 pause
