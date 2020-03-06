3b7f29c0cd97a5057143b9ee7acf5e237adea915
RJK-Engineering <rjklinkhamer@gmail.com>
2020-03-06 01:13:09 +0100

    echo command

--- bat/rjk-clone.bat
+++ bat/rjk-clone.bat
@@ -2,6 +2,7 @@
 if "%~1" == "" echo Clone RJK repository & echo USAGE: %0 [repo] & goto END
 set REPO=%1
 set USER=RJK-Engineering
+echo on
 git clone %3 %4 %5 https://www.github.com/%USER%/%REPO%.git
 :END
 
