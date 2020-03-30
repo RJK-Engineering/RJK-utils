6c2793c2b74aac84044c337c823134256b7cfc8d
RJK-Engineering <rjklinkhamer@gmail.com>
2020-03-30 21:48:05 +0200

    added default user

--- bat/rjk-clone.bat
+++ bat/rjk-clone.bat
@@ -1,8 +1,13 @@
 @echo off
-if "%~1" == "" echo Clone RJK repository & echo USAGE: %0 [repo] & goto END
-set REPO=%1
+
 set USER=RJK-Engineering
+
+if "%~1" == "" echo Clone repository, default user=%USER% & echo USAGE: %0 [repo] [user] [git clone options] & goto END
+
+set REPO=%1
+if not "%~2" == "" set USER=%2
+
 echo on
-git clone %3 %4 %5 https://www.github.com/%USER%/%REPO%.git
+git clone https://www.github.com/%USER%/%REPO%.git %3 %4 %5
 :END
 
