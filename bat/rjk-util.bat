f6c3e1ac36ddc5bbed4db60599db27398ebb8481
RJK-Engineering <rjklinkhamer@gmail.com>
2020-04-23 00:17:21 +0200

    added RJK_UTIL_NOPAUSEONERROR

--- bat/rjk-util.bat
+++ bat/rjk-util.bat
@@ -5,7 +5,7 @@ if not defined RJK_UTILS_HOME FOR /F "delims=" %%P IN ("%~dp0..") DO set RJK_UTI
 perl "%RJK_UTILS_HOME%\utils\%RJK_UTIL%" %*
 
 if %errorlevel% gtr 0. (
-    pause
+    if not defined RJK_UTIL_NOPAUSEONERROR pause
 ) else if defined COMMANDER_INI (
     if not defined RJK_UTIL_NOPAUSE pause
 )
