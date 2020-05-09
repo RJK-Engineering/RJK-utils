cb424b5bf4be3b792319b561f339783d2d55a795
RJK-Engineering <rjklinkhamer@gmail.com>
2020-05-09 08:17:06 +0200

    added ffmerge.pl

---
+++ utils/tbmtv/ffmerge.pl
@@ -0,0 +1,36 @@
+use strict;
+use warnings;
+
+use RJK::File::PathInfo qw(basename extension);
+
+my ($listFile, @ffargs) = @ARGV;
+die "List file required" if ! $listFile;
+
+my %opts = (
+    run => 1,
+    quiet => 0,
+);
+
+mergeStreams();
+
+sub mergeStreams {
+    open my $fh, '<', $listFile or die "$!";
+    my @in;
+    while (<$fh>) {
+        chomp;
+        push @in, -i => $_;
+    }
+    close $fh;
+
+    runffmpeg(@in, @ffargs);
+}
+
+sub runffmpeg {
+    if (! $opts{quiet}) {
+        my @args = map { /^-/ ? $_ : "\"$_\"" } @_;
+        print "ffmpeg @args\n";
+    }
+    if ($opts{run}) {
+        system "ffmpeg", @_;
+    }
+}
