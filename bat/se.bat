fc2b9dd3b58086ac2a32bddf20a684414550ba40
RJK-Engineering <rjklinkhamer@gmail.com>
2020-05-10 13:28:41 +0200

    update

--- bat/se.bat
+++ bat/se.bat
@@ -2,26 +2,37 @@
 
 REM switch between environments
 
-set _to_env=
+set _to_env=%1
+set _arg=%1
 
-if /i "%cd:~0,12%"=="c:\workspace" (
-    set _subdirs=%cd:~13%
-    set _to_env=scripts
-    set PATH=%PATH:c:\workspace\RJK-utils\bat;=c:\scripts\RJK-utils\bat;%
-    set PERL5LIB=%PERL5LIB:c:\workspace\RJK-perl5lib\lib;=c:\scripts\RJK-perl5lib\lib;%
+if not defined _to_env (
+    if /i "%cd:~0,12%"=="c:\workspace" (
+        set _subdirs=%cd:~13%
+        set _to_env=scripts
+    )
+    if /i "%cd:~0,10%"=="c:\scripts" (
+        set _subdirs=%cd:~11%
+        set _to_env=workspace
+    )
 )
-if /i "%cd:~0,10%"=="c:\scripts" (
-    set _to_env=workspace
-    set _subdirs=%cd:~11%
+
+if "%_to_env%"=="workspace" (
     set PATH=%PATH:c:\scripts\RJK-utils\bat;=c:\workspace\RJK-utils\bat;%
     set PERL5LIB=%PERL5LIB:c:\scripts\RJK-perl5lib\lib;=c:\workspace\RJK-perl5lib\lib;%
 )
+if "%_to_env%"=="scripts" (
+    set PATH=%PATH:c:\workspace\RJK-utils\bat;=c:\scripts\RJK-utils\bat;%
+    set PERL5LIB=%PERL5LIB:c:\workspace\RJK-perl5lib\lib;=c:\scripts\RJK-perl5lib\lib;%
+)
 
 if defined _to_env (
-    cd "c:\%_to_env%\%_subdirs%"
     set RJK_UTILS_HOME=c:\%_to_env%\RJK-utils
 ) else (
     echo Not in environment
+)
+
+if not defined _arg (
+    cd "c:\%_to_env%\%_subdirs%"
 )
 
 echo PATH=%PATH%
