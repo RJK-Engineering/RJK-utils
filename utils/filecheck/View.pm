9942a29aa5a78618fcc772d2957fb6257651c27c
RJK-Engineering <rjklinkhamer@gmail.com>
2020-08-03 11:20:22 +0200

    Update View.pm

--- utils/filecheck/View.pm
+++ utils/filecheck/View.pm
@@ -13,26 +13,79 @@ sub new {
 
 sub showMessage {
     my ($self, $message) = @_;
-    print "$message\n";
+    print _formatMessage($message), "\n";
 }
 
-sub showParitionSearchStart {
-    my ($self, $partition) = @_;
-    print "╭╴$partition\n";
+sub showSearchStart {
+    my ($self, $tcSearch, $message) = @_;
+    print _formatMessage($message), "\n" if $message;
 }
 
-sub showParitionSearchDone {
-    my ($self, $partition, $size) = @_;
-    printf "╰╴%s\n", format_bytes($size);
+sub showSearchDone {
+    my ($self, $tcSearch, $stats, $message) = @_;
+    print _formatMessage($message), "\n" if $message;
+}
+
+sub showPartitionSearchStart {
+    my ($self, $partition, $message) = @_;
+    printf "╭╴%s\n", $partition;
+    printf "╞> %s\n", _formatMessage($message) if $message;
+}
+
+sub showPartitionSearchDone {
+    my ($self, $partition, $stats, $message) = @_;
+    printf "╞> %s\n", _formatMessage($message) if $message;
+    printf "╰╴%s\n", format_bytes($stats->{size});
+}
+
+sub showDirSearchStart {
+    my ($self, $dir, $stat, $message) = @_;
+    $self->{dir} = $dir;
+    $self->{dirMessage} = $message;
+
+    if ($message) {
+        $self->_showDirHeader;
+    } else {
+        $self->{shownDirHeader} = 0;
+    }
+}
+
+sub showDirSearchDone {
+    my ($self, $dir, $stats, $message) = @_;
+
+    printf "│╰╴%s\n", $dir->{path} if $self->{shownDirHeader};
+    printf "╞> %s\n", _formatMessage($message) if $message;
 }
 
 sub showResult {
-    my ($self, $file, $stat, $result) = @_;
+    my ($self, $file, $stat, $message) = @_;
+
     if ($stat->{isDir}) {
-        printf "│%4.4s %s\n", "", $file->{path};
-    } else {
-        printf "│%4.4s %s\n", format_bytes($stat->{size}), $file->{path};
+        printf "│%s\n", $file->{path};
+        printf "╞> %s\n", _formatMessage($message) if $message;
+        return;
     }
+
+    $self->_showDirHeader if ! $self->{shownDirHeader};
+    printf "││%4.4s %s\n", format_bytes($stat->{size}), $file->{path};
+    printf "│╞> %s\n", _formatMessage($message) if $message;
+}
+
+sub _showDirHeader {
+    my $self = shift;
+    printf "├┬╴%s\n", $self->{dir}{path};
+    printf "│╞> %s\n", _formatMessage($self->{dirMessage}) if $self->{dirMessage};
+    $self->{shownDirHeader} = 1;
+}
+
+sub _formatMessage {
+    my $message = shift;
+    return $message if ! ref $message;
+    return $message->{error}
+        || $message->{warn}
+        || $message->{info}
+        || $message->{debug}
+        || $message;
 }
 
 1;
