f72f651275e1e898d3f1fa47d37b4a52ffd8e464
RJK-Engineering <rjklinkhamer@gmail.com>
2020-09-05 17:42:24 +0200

    Update ffprobe2avidemux.pl

--- utils/tbmtv/ffprobe2avidemux.pl
+++ utils/tbmtv/ffprobe2avidemux.pl
@@ -6,22 +6,12 @@ use File::Spec::Functions qw(rel2abs);
 my $csv = shift;
 my %opts = @ARGV;
 
+my $script = getScript();
+my $video = getVideo();
+
 open my $fhCsv, '<', $csv or die "$!: $csv";
-
-my $script = $csv =~ s/\.\w+$/.py/r;
-print "Script: $script\n";
-
-my $video = $csv =~ s/\.\w+$/.mp4/r;
-$video = rel2abs($video);
-$video =~ s|\\|\/|g;
-print "Video: $video\n";
-
 open my $fhScript, '>', $script or die "$!: $script";
-print $fhScript "#PY  <- Needed to identify #\n";
-print $fhScript "#--automatically built--\n\n";
-print $fhScript "adm = Avidemux()\n";
-print $fhScript "adm.loadVideo(\"$video\")\n";
-print $fhScript "adm.clearSegments()\n";
+writeScriptStart();
 
 my ($frameNr, $currFrame, $prevFrame, $inSegm, $startTime, $total);
 $opts{frameDuration} || detectFrameDuration();
@@ -37,20 +27,24 @@ while (<$fhCsv>) {
 close $fhCsv;
 stopSegm() if $inSegm;
 
-print $fhScript "adm.markerA = 0\n";
-print $fhScript "adm.markerB = $total\n";
-print $fhScript "adm.videoCodec(\"Copy\")\n";
-print $fhScript "adm.audioClearTracks()\n";
-unless ($opts{noaudio}) {
-    print $fhScript "adm.audioAddTrack(0)\n";
-    print $fhScript "adm.audioCodec(0, \"copy\");\n";
-    print $fhScript "adm.audioSetDrc(0, 0)\n";
-    print $fhScript "adm.audioSetShift(0, 0,0)\n";
-}
-print $fhScript "adm.setContainer(\"MP4V2\", \"optimize=0\", \"add_itunes_metadata=0\")\n";
+writeScriptEnd();
 close $fhScript;
 
 print "\nTotal: $total\n";
+
+sub getScript {
+    my $script = $csv =~ s/\.\w+$/.py/r;
+    print "Script: $script\n";
+    return $script;
+}
+
+sub getVideo {
+    my $video = $csv =~ s/\.\w+$/.mp4/r;
+    $video = rel2abs($video);
+    $video =~ s|\\|\/|g;
+    print "Video: $video\n";
+    return $video;
+}
 
 sub detectFrameDuration {
     my %gaps;
@@ -98,6 +92,28 @@ sub gap {
 sub startSegm {
     $inSegm = 1;
     $startTime = $currFrame->{time};
+}
+
+sub writeScriptStart {
+    print $fhScript "#PY  <- Needed to identify #\n";
+    print $fhScript "#--automatically built--\n\n";
+    print $fhScript "adm = Avidemux()\n";
+    print $fhScript "adm.loadVideo(\"$video\")\n";
+    print $fhScript "adm.clearSegments()\n";
+}
+
+sub writeScriptEnd {
+    print $fhScript "adm.markerA = 0\n";
+    print $fhScript "adm.markerB = $total\n";
+    print $fhScript "adm.videoCodec(\"Copy\")\n";
+    print $fhScript "adm.audioClearTracks()\n";
+    unless ($opts{noaudio}) {
+        print $fhScript "adm.audioAddTrack(0)\n";
+        print $fhScript "adm.audioCodec(0, \"copy\");\n";
+        print $fhScript "adm.audioSetDrc(0, 0)\n";
+        print $fhScript "adm.audioSetShift(0, 0,0)\n";
+    }
+    print $fhScript "adm.setContainer(\"MP4V2\", \"optimize=0\", \"add_itunes_metadata=0\")\n";
 }
 
 sub stopSegm {
