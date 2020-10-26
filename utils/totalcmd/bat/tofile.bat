bd7b0b434de77eedcb5f3b77d44283f001c31def
RJK-Engineering <rjklinkhamer@gmail.com>
2020-10-26 22:55:35 +0100

    Update tofile.bat

--- utils/totalcmd/bat/tofile.bat
+++ utils/totalcmd/bat/tofile.bat
@@ -2,18 +2,22 @@
 setlocal
 
 rem clear vars, they are inherited from master environment
-SET debug=
 SET pause=
+SET nopause=
+SET noerror=
 SET redirect=
+SET debug=
 SET outfile=
 SET command=
 
 :getopt
 IF "%~1"=="" GOTO endgetopt
 IF DEFINED command SET "command=%command% %1" & GOTO nextopt
-IF "%~1"=="/d" SET "debug=1" & GOTO nextopt
 IF "%~1"=="/p" SET "pause=1" & GOTO nextopt
-IF "%~1"=="/r" SET "redirect=1" & GOTO nextopt
+IF "%~1"=="/-p" SET "nopause=1" & GOTO nextopt
+IF "%~1"=="/-e" SET "noerror=2>NUL" & GOTO nextopt
+IF "%~1"=="/r" SET "redirect=2>&1" & GOTO nextopt
+IF "%~1"=="/d" SET "debug=1" & GOTO nextopt
 IF NOT DEFINED outfile SET "outfile=%~1" & GOTO nextopt
 SET command=%1
 :nextopt
@@ -23,18 +27,17 @@ SHIFT & GOTO getopt
 IF DEFINED debug GOTO DEBUG
 
 IF EXIST "%outfile%" ECHO File exists: %outfile% & PAUSE & GOTO END
-IF DEFINED redirect (
-    %command% > "%outfile%" 2>NUL
-) ELSE (
-    %command% > "%outfile%"
-)
+%command% %noerror% %redirect% > %outfile%
 GOTO END
 
 :DEBUG
 ECHO Command: %command%
 ECHO Write output to file: %outfile%
-ECHO Suppress error output: %redirect%
 SET pause=1
 
 :END
+IF %errorlevel% GTR 0 (
+    ECHO Error level %errorlevel%
+    IF NOT DEFINED nopause SET pause=1
+)
 IF DEFINED pause pause
