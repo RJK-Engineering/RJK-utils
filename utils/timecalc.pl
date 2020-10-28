de8a34e9417b10860b8b58de0087a8e1c070aee9
RJK-Engineering <rjklinkhamer@gmail.com>
2020-10-28 22:28:08 +0100

    Create timecalc.pl

---
+++ utils/timecalc.pl
@@ -0,0 +1,17 @@
+use strict;
+use warnings;
+
+use RJK::Time;
+use RJK::TimeFormatter;
+use RJK::TimeParser;
+
+my ($t1, $t2) = @ARGV;
+
+$t1 = RJK::TimeParser->parse($t1);
+$t2 = RJK::TimeParser->parse($t2);
+
+if ($t1->seconds < $t2->seconds) {
+    printf "%s", RJK::TimeFormatter->format($t2->minus($t1));
+} else {
+    printf "%s", RJK::TimeFormatter->format($t1->minus($t2));
+}
