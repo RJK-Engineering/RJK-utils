8a82bb5bf158e8c8fc41d7bec6ce3493c8671ffd
RJK-Engineering <rjklinkhamer@gmail.com>
2020-10-30 13:38:09 +0100

    Update timecalc.pl

--- utils/timecalc.pl
+++ utils/timecalc.pl
@@ -5,13 +5,26 @@ use RJK::Time;
 use RJK::TimeFormatter;
 use RJK::TimeParser;
 
-my ($t1, $t2) = @ARGV;
-
-$t1 = RJK::TimeParser->parse($t1);
-$t2 = RJK::TimeParser->parse($t2);
-
-if ($t1->seconds < $t2->seconds) {
-    printf "%s", RJK::TimeFormatter->format($t2->minus($t1));
-} else {
-    printf "%s", RJK::TimeFormatter->format($t1->minus($t2));
+my $t1 = RJK::TimeParser->parse(shift);
+while (@ARGV) {
+    my $op = shift;
+    my $t2 = RJK::TimeParser->parse(shift);
+    if ($op eq '+') {
+        $t1 = $t1->plus($t2);
+    } elsif ($op eq '-') {
+        $t1 = $t1->minus($t2);
+    } else {
+        die "Invalid operator: $op";
+    }
 }
+printf "%s", RJK::TimeFormatter->format($t1);
+
+#~ my ($t1, $t2) = @ARGV;
+#~ $t1 = RJK::TimeParser->parse($t1);
+#~ $t2 = RJK::TimeParser->parse($t2);
+
+#~ if ($t1->seconds < $t2->seconds) {
+#~     printf "%s", RJK::TimeFormatter->format($t2->minus($t1));
+#~ } else {
+#~     printf "%s", RJK::TimeFormatter->format($t1->minus($t2));
+#~ }
