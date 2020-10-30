5a36c048a5a55440510bd2192b66d2d8a819a7f3
RJK-Engineering <rjklinkhamer@gmail.com>
2020-10-30 13:44:03 +0100

    Update timecalc.pl

--- utils/timecalc.pl
+++ utils/timecalc.pl
@@ -18,13 +18,3 @@ while (@ARGV) {
     }
 }
 printf "%s", RJK::TimeFormatter->format($t1);
-
-#~ my ($t1, $t2) = @ARGV;
-#~ $t1 = RJK::TimeParser->parse($t1);
-#~ $t2 = RJK::TimeParser->parse($t2);
-
-#~ if ($t1->seconds < $t2->seconds) {
-#~     printf "%s", RJK::TimeFormatter->format($t2->minus($t1));
-#~ } else {
-#~     printf "%s", RJK::TimeFormatter->format($t1->minus($t2));
-#~ }
