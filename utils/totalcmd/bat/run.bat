82c0e18d60c777642d935614a0576627a72caac0
RJK-Engineering <rjklinkhamer@gmail.com>
2020-11-03 01:00:05 +0100

    Update run.bat

--- utils/totalcmd/bat/run.bat
+++ utils/totalcmd/bat/run.bat
@@ -9,6 +9,7 @@ SET nopause=
 SET quiet=
 SET noerror=
 SET redirect=
+SET clip=
 SET debug=
 SET cmd=
 SET args=
@@ -22,6 +23,7 @@ IF "%~1"=="/-p" SET "nopause=1" & GOTO nextopt
 IF "%~1"=="/q" SET "quiet=>NUL" & GOTO nextopt
 IF "%~1"=="/-e" SET "noerror=2>NUL" & GOTO nextopt
 IF "%~1"=="/r" SET "redirect=2>&1" & GOTO nextopt
+IF "%~1"=="/c" SET "clip=|CLIP" & GOTO nextopt
 IF "%~1"=="/d" SET "debug=1" & GOTO nextopt
 IF NOT DEFINED cmd SET "cmd=%1" & (IF "%~x1"==".bat" SET "cmd=call %1") & GOTO nextopt
 SET args=%1
@@ -30,21 +32,18 @@ SHIFT & GOTO getopt
 :endgetopt
 
 IF DEFINED debug (
-    ECHO *: %*
-    ECHO pause: %pause%
-    ECHO nopause: %nopause%
-    ECHO quiet: %quiet%
-    ECHO noerror: %noerror%
-    ECHO redirect: %redirect%
-    ECHO debug: %debug%
-    ECHO cmd: %cmd%
-    ECHO args: %args%
+    ECHO *=%*
+    ECHO pause=%pause% nopause=%nopause%
+    ECHO quiet="%quiet%" noerror="%noerror%" redirect="%redirect%" clip="%clip%"
+    ECHO cmd=%cmd%
+    ECHO args=%args%
     SET pause=1
+    GOTO END
 )
 
 IF NOT DEFINED cmd GOTO USAGE
 
-%cmd% %args% %quiet% %noerror% %redirect%
+%cmd% %args% %quiet% %noerror% %redirect% %clip%
 GOTO END
 
 :HELP
@@ -59,6 +58,7 @@ ECHO./-p  No pause before exit (pauses on error by default).
 ECHO./q   Be quiet (suppress standard output).
 ECHO./-e  No error output (suppress standard error).
 ECHO./r   Redirect standard error output to standard output.
+ECHO./c   Redirect standard output to clipboard.
 ECHO./d   Debug mode.
 IF NOT DEFINED help GOTO END
 
