fbe09339be81e9652b5f5fb07e8006839969d486
RJK-Engineering <rjklinkhamer@gmail.com>
2020-12-23 16:30:05 +0100

    Update take_snapshot.bat

--- utils/tbmtv/bat/take_snapshot.bat
+++ utils/tbmtv/bat/take_snapshot.bat
@@ -1,4 +1,4 @@
-@echo off
+@echo on
 
 set video=%1
 set seconds=%2
@@ -8,17 +8,18 @@ set "dir=%~dp1"
 set "name=%~n1"
 
 if "%seconds%"=="" set seconds=30
+
 if "%hms%"=="" (
     set start=%seconds%
+    set timestr=%seconds%s
 ) else (
-    set start=%hms%
-)
-if "%hms%"=="" (
-    set timestr=%start%s
-) else (
-    set timestr=%start::=.%
+    set start=%hms:.=:%
+    set timestr=%hms::=.%
 )
 
 set "image=%dir%%name%_%timestr%.jpg"
 
-ffmpeg -n -i %video% -ss %start% -vframes 1 "%image%"
+rem width divisible by 2
+rem ffmpeg -n -i %video% -ss %start% -vframes 1 -vf "scale=trunc(oh*a/2+0.5)*2:360" "%image%"
+
+ffmpeg -n -i %video% -ss %start% -vframes 1 -vf "scale=-1:360" "%image%"
