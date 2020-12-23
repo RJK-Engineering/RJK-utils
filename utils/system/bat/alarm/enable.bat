f1db5f5b075738d6e7106050bc6c3db291c1bab8
RJK-Engineering <rjklinkhamer@gmail.com>
2020-12-23 16:43:13 +0100

    Update enable.bat

--- utils/system/bat/alarm/enable.bat
+++ utils/system/bat/alarm/enable.bat
@@ -1,4 +1,5 @@
-@schtasks /change /enable /tn alarm
-@if %errorlevel% gtr 0 pause
-@schtasks /query /v /fo list /tn alarm |find "Start Time"
-@if %errorlevel% gtr 0 pause
+@echo off
+schtasks /change /enable /tn alarm
+if %errorlevel% gtr 0 pause
+schtasks /query /v /fo list /tn alarm |find "Start Time"
+pause
