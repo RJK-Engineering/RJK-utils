a218770f02d5c19655b53d02bd413b4969f9979b
RJK-Engineering <rjklinkhamer@gmail.com>
2021-01-17 14:23:36 +0100

    Update snapshots.pl

--- utils/filecheck/snapshots.pl
+++ utils/filecheck/snapshots.pl
@@ -2,6 +2,7 @@ use strict;
 use warnings;
 
 use RJK::Filecheck::Snapshots;
+use RJK::TotalCmd::Utils;
 
 my %opts = (
     verbose => 1,
@@ -10,5 +11,6 @@ my %opts = (
 
 $opts{path} = shift || die "No path specified";
 $opts{position} = shift // 30;
+$opts{listFile} = $opts{path} if RJK::TotalCmd::Utils->isListFile($opts{path});
 
 RJK::Filecheck::Snapshots->create(\%opts);
