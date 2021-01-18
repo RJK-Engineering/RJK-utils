49b7e0372a9c6838d74c6ff54ce329b2c92a08d4
RJK-Engineering <rjklinkhamer@gmail.com>
2021-01-18 09:36:17 +0100

    Create Display.pm

---
+++ utils/fs/sync/Display.pm
@@ -0,0 +1,46 @@
+package Display;
+
+use strict;
+use warnings;
+
+use RJK::HumanReadable::Size;
+use RJK::Win32::Console;
+
+my $console = new RJK::Win32::Console();
+my $sizeFormatter = 'RJK::HumanReadable::Size';
+my $stats;
+
+sub new {
+    return bless {}, shift;
+}
+
+sub setStats {
+    my $self = shift;
+    $stats = shift;
+}
+
+sub info {
+    my ($self, $message) = @_;
+    $console->newline;
+    $console->updateLine($message);
+    $console->newline;
+}
+
+sub warn {
+    my ($self, $message) = @_;
+    $console->newline;
+    $console->updateLine($message);
+    $console->newline;
+}
+
+sub stats {
+    my ($self, $myStats) = @_;
+    $myStats //= $stats // return;
+    $console->updateLine(
+        sprintf "%s in %s files",
+            $sizeFormatter->get($myStats->{size}),
+            $myStats->{visitFile}
+    );
+}
+
+1;
