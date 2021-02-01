2580d7fe924427e9f11241dffdc71b0c8b951f6d
RJK-Engineering <rjklinkhamer@gmail.com>
2021-02-01 04:32:06 +0100

    Update IndexVisitor.pm

--- utils/filecheck/index/IndexVisitor.pm
+++ utils/filecheck/index/IndexVisitor.pm
@@ -4,6 +4,7 @@ use parent 'RJK::FileVisitor';
 use strict;
 use warnings;
 
+use FileVisitResult;
 use RJK::Filecheck::Dir;
 
 my $conf;
@@ -18,25 +19,44 @@ sub new {
 
 sub preVisitDir {
     my ($self, $dir, $stat) = @_;
+    return FileVisitResult::SKIP_SUBTREE if $self->ignore($dir, $stat);
     $self->{dir} = new RJK::Filecheck::Dir($dir);
+}
+
+sub postVisitFiles {
+    my ($self, $dir, $stat) = @_;
+    $self->{dir}->saveProperties;
 }
 
 sub visitFile {
     my ($self, $file, $stat) = @_;
+    return if $self->ignore($file, $stat);
     print "$file->{path}\n";
 
-    my $t = $conf->getFileTypes($file, $stat);
-    return if !@$t;
+    my $fileTypes = $conf->getFileTypes($file, $stat);
+    return if !@$fileTypes;
+    print "File type: @$fileTypes\n";
+    $file->{type} = $fileTypes;
 
-    print "File type: @$t\n";
-    my $v = $conf->getVisitors($t);
+    $self->visit($fileTypes, $file, $stat);
+}
+
+sub visit {
+    my ($self, $fileTypes, $file, $stat) = @_;
+    my @v = $conf->getVisitors(["general"]);
+    push @v, $conf->getVisitors($fileTypes);
 
     my $props = $self->{dir}->getFileProperties($file->name) // {};
-
-    foreach my $visitor (@$v) {
+    foreach my $visitor (@v) {
         $visitor->visitFile($file, $stat, $props);
     }
     $self->{dir}->setFileProperties($file->name, $props);
+}
+
+sub ignore {
+    my ($self, $file, $stat) = @_;
+    local $_ = $file->{name};
+    /^[.~]/ || /~$/;
 }
 
 1;
