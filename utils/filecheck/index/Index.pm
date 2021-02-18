a20a193f1fcecc94c67554f0db4b97506936d0b9
RJK-Engineering <rjklinkhamer@gmail.com>
2021-02-18 19:50:12 +0100

    Update Index.pm

--- utils/filecheck/index/Index.pm
+++ utils/filecheck/index/Index.pm
@@ -5,17 +5,39 @@ use warnings;
 
 use Conf;
 use IndexVisitor;
+use DBD::mysql;
+use RJK::DbTable;
+use RJK::Filecheck::Config;
 use RJK::Files;
 use RJK::Options::Util;
 
 sub execute {
     my $self = shift;
     my $conf = new Conf(shift);
+    $conf->{chdir} = 1;
 
     my $stats = RJK::Files->createStats();
     my $visitor = new IndexVisitor($conf, $stats);
-    $conf->{chdir} = 1;
+    dbConnect();
+
     RJK::Options::Util->traverseFiles($conf, $visitor, $stats);
+    RJK::DbTable->commit();
+}
+
+sub dbConnect {
+    RJK::DbTable->connect(
+        host => RJK::Filecheck::Config->get('db.host.name'),
+        db   => RJK::Filecheck::Config->get('db.database.name'),
+        user => RJK::Filecheck::Config->get('db.user.name'),
+        pass => RJK::Filecheck::Config->get('db.password'),
+        port => RJK::Filecheck::Config->get('db.port', 3306),
+        eventHandlers => {
+            onError => sub {
+                print "ERROR: $_[0]\n";
+                exit;
+            },
+        }
+    );
 }
 
 1;
