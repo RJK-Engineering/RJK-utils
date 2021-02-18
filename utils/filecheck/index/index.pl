9231918e66501c098e05ee5a2211365561728251
RJK-Engineering <rjklinkhamer@gmail.com>
2021-02-18 19:49:30 +0100

    Update index.pl

--- utils/filecheck/index/index.pl
+++ utils/filecheck/index/index.pl
@@ -1,16 +1,16 @@
 use strict;
 use warnings;
 
-use File::Basename;
-use lib dirname (__FILE__);
-
 use RJK::LocalConf;
 
 my %opts = RJK::LocalConf::GetOptions("RJK-utils/filecheck/index.json");
 
 $opts{listFile} = shift;
 
+use File::Basename;
+use lib dirname (__FILE__);
 use Index;
+
 use RJK::Exceptions;
 use Try::Tiny;
 
