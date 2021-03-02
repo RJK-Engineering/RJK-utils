a771032e91f85c941f6fda9aa8a31b3d8c98095e
RJK-Engineering <rjklinkhamer@gmail.com>
2021-03-02 22:02:52 +0100

    Update edit.bat

--- utils/totalcmd/bat/edit.bat
+++ utils/totalcmd/bat/edit.bat
@@ -1,25 +1,24 @@
 @echo off
 if "%EDITOR%"=="" set EDITOR=c:\progz\SciTE\SciTE.exe
 
-if not "%2" == "" (
-    goto END
-) else if "%~1" == "" (
+if "%~1"=="" (
     goto END
 ) else if exist "%~1" (
-    start "" /b "%EDITOR%" %*
-) else (
-    goto SEARCH
+    start "" /b "%EDITOR%" %1
+    goto END
 )
-goto END
 
-:SEARCH
 set found=0
-for /f %%F in ('where %1') do (
-    start "" /b "%EDITOR%" %%F
+for /f "delims=" %%F in ('where %1') do (
+    if /i not "%%~xF"==".exe" if /i not "%%~xF"==".com" start "" /b "%EDITOR%" "%%F"
     set found=1
 )
+if %found%==1 goto END
 
-:NEW
-if %found%==0 start "" /b "%EDITOR%" %*
+if "%2"=="new" (
+    start "" /b "%EDITOR%" %1
+) else (
+    echo Add "new" to open new file.
+)
 
 :END
