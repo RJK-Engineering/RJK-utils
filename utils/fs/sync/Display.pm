dda4bd2cfb8d3e0a3399c3b5dcd9ebb2d9b1ebc0
RJK-Engineering <rjklinkhamer@gmail.com>
2021-03-16 16:01:44 +0100

    Update Display.pm

--- utils/fs/sync/Display.pm
+++ utils/fs/sync/Display.pm
@@ -5,18 +5,29 @@ use warnings;
 
 use RJK::HumanReadable::Size;
 use RJK::Win32::Console;
+use Time::HiRes ();
 
 my $console = new RJK::Win32::Console();
 my $sizeFormatter = 'RJK::HumanReadable::Size';
+my $opts;
 my $stats;
+my $progress;
+my $lastDisplay = 0;
 
 sub new {
-    return bless {}, shift;
+    my $self = bless {}, shift;
+    $opts = shift;
+    return $self;
 }
 
 sub setStats {
     my $self = shift;
     $stats = shift;
+}
+
+sub setProgressBar {
+    my $self = shift;
+    $progress = shift;
 }
 
 sub info {
@@ -29,14 +40,51 @@ sub warn {
     $console->printLine($message);
 }
 
+sub start {
+    $progress->{done} = 0;
+    &_progress;
+}
+
+sub progress {
+    return if timer();
+    &_progress;
+}
+
+sub done {
+    &_progress;
+}
+
+sub _progress {
+    $console->updateLine(
+        sprintf "%u%% %u of %u",
+            $progress->{done} && $progress->{done} / $progress->{total} * 100,
+            $progress->{done},
+            $progress->{total}
+    );
+}
+
 sub stats {
-    my ($self, $myStats) = @_;
-    $myStats //= $stats // return;
+    return if timer();
+    &_stats;
+}
+
+sub totals {
+    &_stats;
+}
+
+sub _stats {
     $console->updateLine(
         sprintf "%s in %s files",
-            $sizeFormatter->get($myStats->{size}),
-            $myStats->{visitFile}
+            $sizeFormatter->get($stats->{size}),
+            $stats->{files}
     );
+}
+
+sub timer {
+    my $time = Time::HiRes::gettimeofday;
+    return 1 if $lastDisplay > $time - $opts->{refreshInterval};
+    $lastDisplay = $time;
+    return 0;
 }
 
 1;
