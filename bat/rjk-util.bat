7cb26c2413901b6d025cb331a32e811f0dd28915
RJK-Engineering <rjklinkhamer@gmail.com>
2021-07-15 17:30:53 +0200

    Update rjk-util.bat

--- bat/rjk-util.bat
+++ bat/rjk-util.bat
@@ -1,38 +1,48 @@
 @echo off
 setlocal
 
-IF NOT DEFINED RJK_UTILS_HOME FOR /F "delims=" %%P IN ("%~dp0..") DO SET RJK_UTILS_HOME=%%~dpfP
+IF NOT DEFINED RJK_UTILS_HOME (
+    FOR /F "delims=" %%P IN ("%~dp0..") DO SET RJK_UTILS_HOME=%%~fP
+)
+SET script=%0
 
 rem clear vars, they are inherited from master environment
-SET debug=
 SET workspaceenvironment=
 SET pause=
 SET nopause=
+SET quiet=
+SET errorredirect=
 SET timeout=
 SET clip=
+SET output=
 SET util=
 SET extension=
 SET args=
-SET timeoutarg=
 
 :getopt
 IF "%~1"=="" GOTO endgetopt
 IF DEFINED args SET args=%args% %1& GOTO nextopt
-IF DEFINED timeoutarg SET timeout=%1& SET timeoutarg=& GOTO nextopt
 IF "%~1"=="/?" GOTO USAGE
-IF "%~1"=="/d" SET debug=1& GOTO nextopt
 IF "%~1"=="/w" SET workspaceenvironment=1& GOTO nextopt
 IF "%~1"=="/p" SET pause=1& GOTO nextopt
 IF "%~1"=="/-p" SET nopause=1& GOTO nextopt
 IF "%~1"=="/n" SET nopause=1& GOTO nextopt
-IF "%~1"=="/t" SET timeoutarg=1& GOTO nextopt
+IF "%~1"=="/q" SET "quiet=>NUL"& GOTO nextopt
+IF "%~1"=="/-e" SET "errorredirect=2>NUL"& GOTO nextopt
+IF "%~1"=="/r" SET "errorredirect=2>&1"& GOTO nextopt
+IF "%~1"=="/t" SET timeout=%2& SHIFT& GOTO nextopt
 IF "%~1"=="/c" SET "clip=|CLIP"& GOTO nextopt
+IF "%~1"=="/o" SET output=%2& SHIFT& GOTO nextopt
 IF NOT DEFINED util (
     SET util=%1
     SET extension=%~x1
     GOTO nextopt
 )
 SET args=%1
+IF "%args:~0,1%"=="/" (
+    ECHO Invalid option: %1
+    GOTO ERROR
+)
 :nextopt
 SHIFT & GOTO getopt
 :endgetopt
@@ -49,15 +59,21 @@ SET cmd=
 IF "%extension%"==".pl" SET cmd=perl
 IF "%extension%"==".bat" SET cmd=call
 
-IF DEFINED debug (
-    echo %cmd% %RJK_UTILS_HOME%\utils\%util% %args%
-) else (
-    %cmd% %RJK_UTILS_HOME%\utils\%util% %args% %clip%
+IF DEFINED output (
+    SET "output=> %output%"
+) ELSE (
+    SET "output=%quiet% %clip%"
 )
+
+%cmd% %RJK_UTILS_HOME%\utils\%util% %args% %errorredirect% %output%
 GOTO END
 
+:ERROR
+SET pause=1
+ECHO.
+
 :USAGE
-ECHO USAGE: %0 [UTIL] [OPTIONS] [ARGS]
+ECHO USAGE: %script% [UTIL] [OPTIONS] [ARGS]
 
 :END
 IF %errorlevel% GTR 0 (
