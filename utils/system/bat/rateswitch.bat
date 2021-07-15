bb03bf963cc8e5bb195d33eba0c834db917870df
RJK-Engineering <rjklinkhamer@gmail.com>
2021-07-16 01:17:18 +0200

    Update rateswitch.bat

--- utils/system/bat/rateswitch.bat
+++ utils/system/bat/rateswitch.bat
@@ -9,16 +9,16 @@ set cur_rate=%KEYBOARD_CHAR_REPEAT_CURR_RATE%
 set def_rate=%KEYBOARD_CHAR_REPEAT_DEFAULT_RATE%
 if not defined def_rate set def_rate=31
 
+set rate=
 if defined value (
     set rate=%value%
     call set KEYBOARD_CHAR_REPEAT_PRESET_%name%=%value%
     call setx KEYBOARD_CHAR_REPEAT_PRESET_%name% %value% >NUL
-) else if not "%cur_rate%"=="%def_rate%" (
-    set rate=%def_rate%
-) else (
-    call set rate=%%KEYBOARD_CHAR_REPEAT_PRESET_%name%%%
+) else if defined switch (
+    if not "%cur_rate%"=="%def_rate%" set rate=%def_rate%
 )
 
+if not defined rate call set rate=%%KEYBOARD_CHAR_REPEAT_PRESET_%name%%%
 if not defined rate (
     echo KEYBOARD_CHAR_REPEAT_PRESET_%name% not defined.
     set ERRORLEVEL=1
