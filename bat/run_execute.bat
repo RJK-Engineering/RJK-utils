21ee76ca45992fbf021521f6632351ae62e22eb2
RJK-Engineering <rjklinkhamer@gmail.com>
2021-08-01 01:18:39 +0200

    Update run_execute.bat

--- bat/run_execute.bat
+++ bat/run_execute.bat
@@ -1,7 +1,25 @@
 
-IF DEFINED call SET cmd=%call% %cmd%
-IF DEFINED args SET "cmd=%cmd% "
-IF DEFINED errorredirect SET "errorredirect= %errorredirect%"
+:SET_RUN
+IF /I "%extension%"==".pl" SET run= PERL
+IF /I "%extension%"==".bat" SET run= CALL
+IF DEFINED background SET run= START /B%run%
+SET run=%run% %cmd%
+SET run=%run:~1%
 
-IF DEFINED echo ECHO "%cmd%%args%%quiet%%output%%errorredirect%%clip%"
-%cmd%%args%%quiet%%output%%errorredirect%%clip%
+:SET_OUT
+IF DEFINED append (
+    SET "out=>>%append%"
+) ELSE IF DEFINED output (
+    CALL run_check_output %output%
+    SET "out=>%output%"
+)
+IF DEFINED errorredirect SET "out=%out% %errorredirect%"
+SET "out=%quiet%%out%%clip%"
+
+:EXECUTE
+IF DEFINED echo ECHO run=%run%& ECHO arg=%args%& ECHO out="%out%"
+IF DEFINED args (
+    %run% %args%%out%
+) ELSE (
+    %run%%out%
+)
