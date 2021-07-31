50a9239e67f9096ac4ea6a71130aef57168b3ce1
RJK-Engineering <rjklinkhamer@gmail.com>
2021-07-31 10:02:59 +0200

    Update batch.bat

--- utils/totalcmd/bat/batch.bat
+++ utils/totalcmd/bat/batch.bat
@@ -1,66 +1,57 @@
 @ECHO OFF
 SETLOCAL
 
+SET script=%0
 IF "%~1"=="" GOTO USAGE
 
-rem clear vars, they are inherited from master environment
-SET display=
-SET pause=
-SET nopause=
-SET quiet=
-SET noerror=
-SET redirect=
-SET debug=
-SET filelist=
-SET cmd=
-SET args=
+REM clear vars, they are inherited from master environment
+FOR %%V in (cmd filelist args display pause nopause timeout quiet errorredirect clip output call) do set %%V=
 
 :getopt
 IF "%~1"=="" GOTO endgetopt
-IF DEFINED args SET args=%args% %1& GOTO nextopt
+IF DEFINED args SET args=%args% %1&        GOTO nextopt
 IF "%~1"=="/?" GOTO HELP
-IF "%~1"=="/f" SET display=%%~fF& GOTO nextopt
-IF "%~1"=="/n" SET display=%%~nxF& GOTO nextopt
-IF "%~1"=="/p" SET pause=1& GOTO nextopt
-IF "%~1"=="/-p" SET nopause=1& GOTO nextopt
-IF "%~1"=="/q" SET "quiet=>NUL"& GOTO nextopt
-IF "%~1"=="/-e" SET "noerror=2>NUL"& GOTO nextopt
-IF "%~1"=="/r" SET "redirect=2>&1"& GOTO nextopt
-IF "%~1"=="/d" SET debug=1& GOTO nextopt
-IF NOT DEFINED cmd SET cmd=%1& (IF "%~x1"==".bat" SET cmd=call %1) & GOTO nextopt
-IF NOT DEFINED filelist SET filelist=%1& GOTO nextopt
-SET args=%1
+IF "%~1"=="/f"  SET display=%%~fF&         GOTO nextopt
+IF "%~1"=="/n"  SET display=%%~nxF&        GOTO nextopt
+IF "%~1"=="/p"  SET pause=1& SET nopause=& GOTO nextopt
+IF "%~1"=="/-p" SET nopause=1& SET pause=& GOTO nextopt
+IF "%~1"=="/t"  SET timeout=%2&    SHIFT & GOTO nextopt
+IF "%~1"=="/q"  SET "quiet=>NUL"         & GOTO nextopt
+IF "%~1"=="/-e" SET "errorredirect=2>NUL"& GOTO nextopt
+IF "%~1"=="/r"  SET "errorredirect=2>&1" & GOTO nextopt
+IF "%~1"=="/c"  SET "clip=|CLIP"         & GOTO nextopt
+IF "%~1"=="/a"  SET "append=>>%2"& SHIFT & GOTO nextopt
+IF "%~1"=="/-"  SET args=%2&       SHIFT & GOTO nextopt
+IF NOT DEFINED cmd (
+    SET cmd=%1
+    SET extension=%~x1
+) ELSE IF NOT DEFINED filelist (
+    SET filelist=%1
+) ELSE (
+    SET args=%1
+)
 :nextopt
 SHIFT & GOTO getopt
 :endgetopt
 
-IF DEFINED debug (
-    ECHO *=%*
-    ECHO display=%display% pause=%pause% nopause=%nopause%
-    ECHO quiet="%quiet%" noerror="%noerror%" redirect="%redirect%"
-    ECHO filelist=%filelist%
-    ECHO cmd=%cmd%
-    ECHO args=%args%
-    SET pause=1
-)
-
 IF NOT DEFINED filelist GOTO USAGE
 
+IF "%extension%"==".pl"  SET call=perl
+IF "%extension%"==".bat" SET call=call
+
+IF DEFINED call SET cmd=%call% %cmd%
+IF DEFINED errorredirect SET "errorredirect= %errorredirect%"
+
 FOR /F "tokens=*" %%F IN (%filelist%) DO (
-    IF DEFINED debug (
-        ECHO %%~fF
-        ECHO %cmd% %args%
-    ) ELSE (
-        IF DEFINED display ECHO %display%
-        %cmd% %args% %quiet% %noerror% %redirect%
-    )
+    IF DEFINED display ECHO %display%
+    %cmd% %args%%quiet%%append%%errorredirect%%clip%
 )
 GOTO END
 
 :HELP
 SET help=1
 :USAGE
-ECHO USAGE: %0 [OPTIONS] [COMMAND] [OPTIONS] [FILELIST] [OPTIONS] [ARGS]
+ECHO USAGE: %script% [OPTIONS] [COMMAND] [OPTIONS] [FILELIST] [OPTIONS] [ARGS]
 ECHO.
 ECHO Execute COMMAND with ARGS for every file in FILELIST.
 ECHO %%F in COMMAND or ARGS expands to the current file.
@@ -69,12 +60,15 @@ ECHO OPTIONS
 ECHO./?   Display extended help.
 ECHO./f   Display fully qualified path name before each execution.
 ECHO./n   Display file name before each execution.
-ECHO./p   Pause before exit (only pauses on error by default, overrides /-p).
-ECHO./-p  No pause before exit (pauses on error by default).
-ECHO./q   Be quiet (suppress standard output).
-ECHO./-e  No error output (suppress standard error).
-ECHO./r   Redirect standard error output to standard output.
-ECHO./d   Debug mode.
+ECHO./p        Force pause before exit
+ECHO./-p       Force no pause before exit (pauses on error by default)
+ECHO./t [n]    Timeout before exit
+ECHO./q        Be quiet (supress standard output)
+ECHO./-e       Hide errors (supress error output)
+ECHO./r        Redirect error output to standard output
+ECHO./c        Redirect standard output to clipboard
+ECHO /a [path] Append standard output to file
+ECHO /-        OPTIONS terminator, rest of command line are ARGS
 IF NOT DEFINED help GOTO END
 
 ECHO.
@@ -107,8 +101,4 @@ ECHO.
 ECHO (source: http://www.microsoft.com/resources/documentation/windows/xp/all/proddocs/en-us/percent.mspx)
 
 :END
-IF %errorlevel% GTR 0 (
-    ECHO Error level %errorlevel%
-    IF NOT DEFINED nopause SET pause=1
-)
-IF DEFINED pause pause
+call run_end
