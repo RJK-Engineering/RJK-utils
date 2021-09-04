2fb8a4288a90893f342191b899e933efab94d598
RJK-Engineering <rjklinkhamer@gmail.com>
2021-09-04 23:55:57 +0200

    Update ListMultipleNames.pm

--- utils/filecheck/dirs/Actions/ListMultipleNames.pm
+++ utils/filecheck/dirs/Actions/ListMultipleNames.pm
@@ -13,15 +13,18 @@ sub execute {
     my $self = shift;
     $opts = shift;
     my @dirs;
+    my $results;
 
     RJK::Filecheck::DirLists->traverse($opts->{list}, sub {
         my $vpath = shift;
         my $names = Utils::getNames($vpath);
         if (@$names > 1) {
             dd \@$names;
+            $results++;
         }
         return 0;
     });
+    return $results;
 }
 
 1;
