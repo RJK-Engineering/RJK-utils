78babb0a611d43670493ece1b1e64b2c3b7712dc
RJK-Engineering <rjklinkhamer@gmail.com>
2021-09-04 23:54:50 +0200

    Update Utils.pm

--- utils/filecheck/dirs/Utils.pm
+++ utils/filecheck/dirs/Utils.pm
@@ -3,22 +3,13 @@ package Utils;
 use strict;
 use warnings;
 
-use RJK::Env;
-
 sub getNames {
     my ($path) = @_;
-    my $basename = $path->basename;
-    while ($basename =~ s/\s*\(.*\)\s*//g) {};
-    while ($basename =~ s/\s*\[.*\]\s*//g) {};
-    while ($basename =~ s/\s*\{.*\}\s*//g) {};
-    return [ split /\s*\W+\s+/, $basename ];
-}
-
-sub getEmptyDir {
-    my ($tempDir) = @_;
-    my $emptyDir = RJK::Env->subst("$tempDir\\z");
-    -e $emptyDir || mkdir $emptyDir or die "Could not create directory: $!";
-    return $emptyDir;
+    my $name = $path->name;
+    while ($name =~ s/\s*\(.*\)\s*//g) {};
+    while ($name =~ s/\s*\[.*\]\s*//g) {};
+    while ($name =~ s/\s*\{.*\}\s*//g) {};
+    return [ split /\s*\W+\s+/, $name ];
 }
 
 1;
