ec3c7873ad0d89eb8f3d6869b3b88b58b7bb2688
RJK-Engineering <rjklinkhamer@gmail.com>
2021-09-30 13:22:43 +0200

    Update FindDupes.pm

--- utils/filecheck/dirs/Actions/FindDupes.pm
+++ utils/filecheck/dirs/Actions/FindDupes.pm
@@ -12,23 +12,28 @@ sub execute {
     my $self = shift;
     $opts = shift;
     my %dirs;
-    my $results;
+    my $result = 1;
+    my $dupes;
 
     RJK::Filecheck::DirLists->traverse($opts->{list}, sub {
         my $vpath = shift;
         my $names = Utils::getNames($vpath);
 
         foreach my $name (@$names) {
-            my $dir = $opts->{dupesSameVolume} ? $dirs{$vpath->volume}{$name} //= [] : $dirs{$name} //= [];
-            if (@$dir) {
-                print "--\n$dir->[0]\n" if @$dir == 1;
-                print "$vpath\n";
-                $results++;
-            }
-            push @$dir, $vpath->{path};
+            push @{$dupes->{$name}}, $vpath;
         }
     });
-    return $results;
+
+    foreach my $dirs (sort {$a->[0] cmp $b->[0]} values %$dupes) {
+        next if @$dirs == 1;
+        next if $opts->{volume} && ! grep { $_ =~ /^$opts->{volume}/i } @$dirs;
+        print "--\n";
+        foreach (@$dirs) {
+            print "$_\n";
+        }
+    }
+
+    return $result;
 }
 
 1;
