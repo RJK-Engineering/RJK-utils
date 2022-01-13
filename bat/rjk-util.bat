70b550831ea934bc8cdf777b4454a7ef82d4a665
RJK-Engineering <rjklinkhamer@gmail.com>
2022-01-13 15:47:12 +0100

    Update rjk-util.bat

--- bat/rjk-util.bat
+++ bat/rjk-util.bat
@@ -8,19 +8,11 @@ IF NOT DEFINED RJK_UTILS_HOME (
 CALL run_start %0 "%~1"
 IF defined help GOTO HELP
 
-SET option/w=
 CALL run_getopt %*
-
 SET cmd=%arg1%
 IF not defined cmd GOTO HELP
 SET extension=%ext1%
 SET args=%args1%
-
-IF DEFINED option/w (
-    SET "RJK_UTILS_HOME=c:\workspace\RJK-utils%"
-    SET "PATH=%PATH:c:\scripts\RJK-utils\bat=c:\workspace\RJK-utils\bat%"
-    SET "PERL5LIB=%PERL5LIB:c:\scripts\RJK-perl5lib\lib=c:\workspace\RJK-perl5lib\lib%"
-)
 
 SET cmd=%RJK_UTILS_HOME%\utils\%cmd%
 CALL run_execute
@@ -35,7 +27,6 @@ IF "%help%"=="usage" (
 )
 ECHO OPTIONS:
 ECHO./?        Help
-ECHO /w        Workspace environment
 ECHO /p        Force pause before exit
 ECHO /-p       Force no pause before exit
 ECHO /t [n]    Timeout before exit
