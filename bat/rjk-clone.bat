5acdb954cad546777dc3e5e306326119ac718dbd
RJK-Engineering <rjklinkhamer@gmail.com>
2022-01-13 14:33:16 +0100

    Update rjk-clone.bat

--- bat/rjk-clone.bat
+++ bat/rjk-clone.bat
@@ -1,13 +1,14 @@
 @echo off
 
+if "%~1" == "" (
+    echo Clone repository, default user=%USER%
+    echo USAGE: %0 [repo] [user] [git clone options]
+    goto END
+)
+set REPO=%1
 set USER=RJK-Engineering
 
-if "%~1" == "" echo Clone repository, default user=%USER% & echo USAGE: %0 [repo] [user] [git clone options] & goto END
-
-set REPO=%1
-if not "%~2" == "" set USER=%2
-
 echo on
-git clone https://www.github.com/%USER%/%REPO%.git %3 %4 %5
-:END
+git clone https://www.github.com/%USER%/%REPO%.git %2 %3 %4 %5 %6 %7 %8 %9
 
+:END
