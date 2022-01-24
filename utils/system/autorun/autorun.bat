bb14bc84707ba35bdddadc2c5adb217120db6f22
RJK-Engineering <rjklinkhamer@gmail.com>
2022-01-24 22:47:47 +0100

    included check

--- utils/system/autorun/autorun.bat
+++ utils/system/autorun/autorun.bat
@@ -1,15 +1,11 @@
 @ECHO OFF
 
 IF /I "%~1"=="/install" (
-    CALL %~dpn0_check.bat
     GOTO INSTALL
 ) ELSE IF /I "%~1"=="/uninstall" (
-    ECHO Uninstalling cmd.exe auto-run script
-    PAUSE
-    reg delete "HKCU\Software\Microsoft\Command Processor" /v "AutoRun" /f
-    GOTO END
+    GOTO UNINSTALL
 ) ELSE IF /I "%~1"=="/check" (
-    CALL %~dpn0_check.bat
+    CALL :CHECK
     GOTO END
 )
 
@@ -19,8 +15,10 @@ DOSKEY /MACROFILE=%AUTORUN_MACROS%
 SET tempfile=%TEMP%\cmd-autorun-startup-info-lockfile
 IF EXIST %tempfile% GOTO END
 ECHO.> %tempfile%
+GOTO INFO
 
 :INSTALL
+CALL :CHECK
 IF "%installed%"=="%~f0" (
     ECHO Script already installed.
     SET installed=
@@ -30,6 +28,20 @@ ECHO Installing cmd.exe auto-run script %~f0
 IF defined installed ECHO Warning! Replacing currently installed cmd.exe auto-run script: %installed%
 SET installed=
 REG add "HKCU\Software\Microsoft\Command Processor" /v "AutoRun" /t REG_EXPAND_SZ /d "%~f0"
+GOTO INFO
+
+:UNINSTALL
+ECHO Uninstalling cmd.exe auto-run script
+PAUSE
+REG delete "HKCU\Software\Microsoft\Command Processor" /v "AutoRun" /f
+GOTO END
+
+:CHECK
+SET installed=
+FOR /F "tokens=1,3" %%F IN ('reg query "HKCU\Software\Microsoft\Command Processor" /v "AutoRun"') DO (
+    IF "%%F"=="AutoRun" SET installed=%%G
+)
+EXIT /b
 
 :INFO
 ECHO.
@@ -42,7 +54,6 @@ ECHO.
 REM NOTE: CALL is not used because it will result in recursive execution of cmd.exe,
 REM however not using CALL means that the rest of this script will not be executed!
 IF EXIST %~dpn0_local.bat %~dpn0_local.bat
-GOTO END
 
 :END
 IF defined installed (
--- utils/system/autorun/autorun_check.bat
+++
@@ -1,4 +0,0 @@
-SET installed=
-FOR /F "tokens=1,3" %%F IN ('reg query "HKCU\Software\Microsoft\Command Processor" /v "AutoRun"') DO (
-    IF "%%F"=="AutoRun" SET installed=%%G
-)
