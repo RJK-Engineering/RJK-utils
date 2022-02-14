c5ae36fa5b9f7ce896e60cc0845d26be3011dae1
RJK-Engineering <rjklinkhamer@gmail.com>
2022-02-14 19:19:17 +0100

    Update edit.bat

--- utils/totalcmd/bat/edit.bat
+++ utils/totalcmd/bat/edit.bat
@@ -3,24 +3,16 @@ SETLOCAL
 
 IF not defined EDITOR SET EDITOR=c:\progz\SciTE\SciTE.exe
 
-IF "%~1"=="" GOTO END
-IF exist %1 (
-    START "" /b "%EDITOR%" %1
-    GOTO END
-)
+IF "%~1"=="" EXIT/b
+SET file=%1
+IF exist %1 GOTO GO
 
-SET found=
 FOR /f "delims=" %%F IN ('where %1') DO (
     ECHO %%F
-    IF /i not "%%~xF"==".exe" IF /i not "%%~xF"==".com" START "" /b "%EDITOR%" "%%F"
-    SET found=1
-)
-IF defined found GOTO END
-
-IF "%2"=="new" (
-    START "" /b "%EDITOR%" %1
-) ELSE (
-    ECHO Add "new" to open new file.
+    IF /i not "%%~xF"==".exe" IF /i not "%%~xF"==".com" (
+        SET file=%%F
+    )
 )
 
-:END
+:GO
+START "" /b "%EDITOR%" %file%
