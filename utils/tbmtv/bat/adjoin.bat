47b2bb792ddcb36dd7322a269519741dad8e9cc0
RJK-Engineering <rjklinkhamer@gmail.com>
2022-03-06 13:49:05 +0100

    Create adjoin.bat

---
+++ utils/tbmtv/bat/adjoin.bat
@@ -0,0 +1,28 @@
+@echo off
+setlocal
+
+if "%~2"=="" (
+    echo Join files in list file using Avidemux.
+    echo USAGE: %0 [list file] [output file] [optional output format]
+    exit/b 1
+)
+
+set load=
+for /f "delims=" %%F in (%1) do (
+    if defined load (
+        call set load=%%load%% --append %%F
+    ) else (
+        call set load=--load %%F
+    )
+)
+
+set format=%3
+if defined format set format= --output-format %format%
+
+echo avidemux.exe --force-alt-h264 %load%%format% --save %2 --quit
+set abort=
+echo Press enter to continue, enter any text to abort.
+set /p abort=
+if defined abort exit/b
+
+avidemux.exe --force-alt-h264 %load%%format% --save %2 --quit
