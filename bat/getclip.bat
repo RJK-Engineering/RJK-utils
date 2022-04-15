88fbc19355aeb5dba3624d9ef74bc713918e3ec5
RJK-Engineering <rjklinkhamer@gmail.com>
2022-04-15 20:45:21 +0200

    Update getclip.bat

--- bat/getclip.bat
+++ bat/getclip.bat
@@ -3,14 +3,12 @@
 REM display clipboard
 IF "%~1"=="/d" (
     CSCRIPT /nologo /e:JScript "%~dpn0.js"
-    GOTO END
+    EXIT/b
 )
 
 REM set %clip% to first line on clipboard
 SET clip=
 FOR /f "delims=" %%F in ('CSCRIPT /nologo /e:JScript "%~dpn0.js"') DO (
-    SET clip=%%F
-    GOTO END
+    SET "clip=%%F"
+    EXIT/b
 )
-
-:END
