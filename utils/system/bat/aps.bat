8163ec03b487c7e75fe7a43b09430952862790e7
RJK-Engineering <rjklinkhamer@gmail.com>
2022-04-16 17:23:56 +0200

    Update aps.bat

--- utils/system/bat/aps.bat
+++ utils/system/bat/aps.bat
@@ -4,7 +4,7 @@ IF "%~1" == "" GOTO USAGE
 
 REM ~ powercfg /SETACVALUEINDEX 381b4222-f694-41f0-9685-ff5bb260df2e 54533251-82be-4824-96c1-47b60b740d00 bc5038f7-23e0-4960-96da-33abaf5935ec 55
 powercfg /setacvalueindex SCHEME_BALANCED SUB_PROCESSOR PROCTHROTTLEMAX %1
-powercfg /setactive, SCHEME_BALANCED
+powercfg /setactive SCHEME_BALANCED
 CALL setdescr System.bar "Current setting:" "%1%%%%"
 EXIT/B
 
