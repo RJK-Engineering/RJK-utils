863220bff11bfdb1e620cf52e011ff1423682e01
RJK-Engineering <rjklinkhamer@gmail.com>
2022-04-21 12:59:37 +0200

    Update run_log.bat

--- bat/run_log.bat
+++ bat/run_log.bat
@@ -1,9 +1,8 @@
 IF not defined COMMANDER_RUN_LOG EXIT/B
 
-REM %1 MUST NOT END WITH: " 2"
-REM -> this will result in a redirect: " 2>> logfile"
-rem ECHO %DATE% %TIME% %~1>> "%COMMANDER_RUN_LOG%"
-
+REM IMPORTANT: Use a FOR loop to prevent pitfall in "ECHO %1>> file":
+REM When %1 ends with " 1" or " 2", 1 or 2 will be interpreted as
+REM magic number for standard output and standard error: " 1>> file"
 FOR /F "tokens=*" %%F IN ("%DATE% %TIME% %~1") DO (
     ECHO %%F>> "%COMMANDER_RUN_LOG%"
 )
