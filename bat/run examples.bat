6a2b90f7bff6f47cb0c2d36cd664db5ea8941a48
RJK-Engineering <rjklinkhamer@gmail.com>
2022-04-21 13:54:18 +0200

    Update run examples.bat

--- bat/run examples.bat
+++ bat/run examples.bat
@@ -1,4 +1,11 @@
 exit/b
+rem Command line versions!
+rem In a bat file "%%" should be used instead of a single "%".
+
+run /prompt "question:" -- cmd/d/c @echo answer: %~I
 
 rem supported choices: 0..9
-run /O 1="option 1" /O 2="option 2" /OM="a message" -- cmd/d/c @echo %~O
+run /O 1="option 1" /O 2="option 2" -- cmd/d/c @echo %~O
+run /O 1="option 1" /O 2="option 2" /OM="a message:" -- cmd/d/c @echo choice: %~O
+
+run -- cmd/d/c @echo 1st line on clipboard: %L
