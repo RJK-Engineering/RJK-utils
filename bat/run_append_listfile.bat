87df72bd89399479773acd8e67fc2ff5cfcbdbab
RJK-Engineering <rjklinkhamer@gmail.com>
2022-04-21 21:02:53 +0200

    redirect

--- bat/run_append_listfile.bat
+++ bat/run_append_listfile.bat
@@ -5,16 +5,16 @@ SET _args=%args%
 SET args=%args% %listfile%
 
 IF defined appendc (
-    CALL getclip /d > %listfile%
+    >%listfile% CALL getclip /d
 ) ELSE IF defined appendd (
     IF exist %listfile% del/q %listfile%
     IF defined dirpath PUSHD. & cd/d %dirpath%
-    FOR /F "delims=" %%F IN ('dir/a-d/b') DO ECHO %%~fF>> %listfile%
+    FOR /F "delims=" %%F IN ('dir/a-d/b') DO >>%listfile% ECHO %%~fF
     IF defined dirpath POPD
 ) ELSE IF defined appendn (
-    dir/a-d/b %dirpath% > %listfile%
+    >%listfile% dir/a-d/b %dirpath%
 ) ELSE IF defined appends (
-    dir/a-d/b/s %dirpath% > %listfile%
+    >%listfile% dir/a-d/b/s %dirpath%
 ) ELSE (
     SET dellistfile=
     SET args=%_args%
--- bat/run_log.bat
+++ bat/run_log.bat
@@ -1,9 +1,3 @@
 IF defined nolog EXIT/B
 IF not defined COMMANDER_RUN_LOG EXIT/B
-
-REM IMPORTANT: Use a FOR loop to prevent pitfall in "ECHO %*>> file":
-REM when %* ends with " 1" or " 2", 1 or 2 will be interpreted as
-REM magic number for standard output or standard error: " 1>> file"
-FOR /F "tokens=*" %%F IN ("%DATE% %TIME% %*") DO (
-    ECHO %%F>> "%COMMANDER_RUN_LOG%"
-)
+>>"%COMMANDER_RUN_LOG%" ECHO %DATE% %TIME% %*
