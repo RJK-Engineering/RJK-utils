dec9e89521433f37a5541b06991118d69258b5be
RJK-Engineering <rjklinkhamer@gmail.com>
2022-05-15 05:13:02 +0200

    Update aps.bat

--- utils/system/bat/aps.bat
+++ utils/system/bat/aps.bat
@@ -1,15 +1,14 @@
-@ECHO OFF
+@echo off
+if "%~1" == "" goto USAGE
 
-IF "%~1" == "" GOTO USAGE
-
-REM ~ powercfg /SETACVALUEINDEX 381b4222-f694-41f0-9685-ff5bb260df2e 54533251-82be-4824-96c1-47b60b740d00 bc5038f7-23e0-4960-96da-33abaf5935ec 55
+rem ~ powercfg /SETACVALUEINDEX 381b4222-f694-41f0-9685-ff5bb260df2e 54533251-82be-4824-96c1-47b60b740d00 bc5038f7-23e0-4960-96da-33abaf5935ec 55
 powercfg /setacvalueindex SCHEME_BALANCED SUB_PROCESSOR PROCTHROTTLEMAX %1
 powercfg /setactive SCHEME_BALANCED
-CALL setdescr System.bar "Current setting:" "%1%%%%"
-EXIT/B
+call setdescr System.bar "Current setting:" "%1%%%%"
+exit/b
 
 :USAGE
-ECHO USAGE: %0 ^<value^>
-ECHO.
+echo USAGE: %0 ^<value^>
+echo.
 powercfg /query SCHEME_BALANCED SUB_PROCESSOR PROCTHROTTLEMAX
-PAUSE
+pause
