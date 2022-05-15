54efb484032feee3b1b1a7edd11da7b6e7cb416e
RJK-Engineering <rjklinkhamer@gmail.com>
2022-05-15 05:14:06 +0200

    Update aps.bat

--- utils/system/bat/aps.bat
+++ utils/system/bat/aps.bat
@@ -4,7 +4,7 @@ if "%~1" == "" goto USAGE
 rem ~ powercfg /SETACVALUEINDEX 381b4222-f694-41f0-9685-ff5bb260df2e 54533251-82be-4824-96c1-47b60b740d00 bc5038f7-23e0-4960-96da-33abaf5935ec 55
 powercfg /setacvalueindex SCHEME_BALANCED SUB_PROCESSOR PROCTHROTTLEMAX %1
 powercfg /setactive SCHEME_BALANCED
-call setdescr System.bar "Current setting:" "%1%%%%"
+for /f %%F in ("setdescr.bat") do if exist %%~$PATH:F call %%F System.bar "Current setting:" "%1%%%%"
 exit/b
 
 :USAGE
