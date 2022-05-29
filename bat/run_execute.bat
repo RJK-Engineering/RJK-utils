a09264d9be2df721895df0d7da021ade195c399a
RJK-Engineering <rjklinkhamer@gmail.com>
2022-05-29 21:44:03 +0200

    Update run_execute.bat

--- bat/run_execute.bat
+++ bat/run_execute.bat
@@ -1,30 +1,18 @@
 
 IF not "%~1"=="" SET args= %*
+IF defined errorredirect SET "errorredirect= %errorredirect%"
 
-::SET_RUN
-SET run=
-IF /I "%extension%"==".bat" SET run= CALL
-IF /I "%extension%"==".pl" SET run= PERL
-IF defined background SET run= START /B%run%
-SET run=%run% %cmd%
-SET run=%run:~1%
+IF /I "%extension%"==".bat" SET "cmd=CALL %cmd% "
+IF /I "%extension%"==".pl" SET "cmd=PERL %cmd% "
+IF defined background SET cmd=START /B %cmd%
 
-::SET_OUT
 SET out=
-IF defined append SET "out= >>%append%"
+IF defined append SET "out=>>%append%"
 IF defined output CALL run_check_output %output%
 IF defined error EXIT/B
-IF defined output SET "out= >%output%"
-IF defined errorredirect SET "out=%out% %errorredirect%"
-IF defined quiet SET "out= %quiet%%out%"
-SET "out=%out%%toclip%"
+IF defined output SET "out=>%output%"
+IF defined grep SET "out=|FIND /I "%grep%"%out%"
 
-::EXECUTE
-IF defined wait ECHO run=%run%& ECHO args=%args%& ECHO "out=%out%"& PAUSE
-CALL run_log %run%%args%
-
-IF defined grep (
-    %run%%args%|FIND /I "%grep%"%out%
-) ELSE (
-    %run%%args%%out%
-)
+IF defined wait ECHO cmd=%cmd%& ECHO args=%args%& ECHO redir="%quiet%%out%%toclip%"& PAUSE
+CALL run_log %cmd%%args%
+%cmd%%args%%errorredirect%%quiet%%out%%toclip%
