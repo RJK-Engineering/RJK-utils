9e0f518c011b71dce940c4891c2dab3a22c6a73c
RJK-Engineering <rjklinkhamer@gmail.com>
2022-09-09 01:49:30 +0200

    bar was renamed

--- utils/totalcmd/bat/enable-logging.bat
+++ utils/totalcmd/bat/enable-logging.bat
@@ -4,8 +4,8 @@ cscript /nologo "%~dpn0.vbs" "Rob^ - Total Commander"
 
 if "%COMMANDER_CONF_LOG_ENABLED%"=="1" (
     setx COMMANDER_CONF_LOG_ENABLED 0
-    call settcbicon InternalCommands.bar "Conf: Log file" icons\c\red.ico
+    call settcbicon TotalCommander.bar "Conf: Log file" icons\c\red.ico
 ) else (
     setx COMMANDER_CONF_LOG_ENABLED 1
-    call settcbicon InternalCommands.bar "Conf: Log file" icons\c\green.ico
+    call settcbicon TotalCommander.bar "Conf: Log file" icons\c\green.ico
 )
