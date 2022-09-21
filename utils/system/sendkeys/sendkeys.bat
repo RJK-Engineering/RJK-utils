12e88c19f5b481afc9b935c14fb2d5092d6126d1
RJK-Engineering <rjklinkhamer@gmail.com>
2022-09-21 14:34:53 +0200

    Update sendkeys.bat

--- utils/system/sendkeys/sendkeys.bat
+++ utils/system/sendkeys/sendkeys.bat
@@ -1,17 +1,14 @@
 @echo off
 setlocal
 
-set/a start=%TIME:~0,2%*3600 + %TIME:~3,2%*60 + %TIME:~6,2%
+call :time2seconds
+set start=%seconds%
 
 set timeout=%2
 timeout %timeout%
 
-:STOP
-:: this expression sometimes FAILS with "Invalid number."
-set/a stop=%TIME:~0,2%*3600 + %TIME:~3,2%*60 + %TIME:~6,2%
-if not defined stop timeout 1 >NUL & goto STOP
-
-set/a diff=stop-start
+call :time2seconds
+set/a diff=seconds-start
 if %diff% lss 0 set/a diff+=86400
 
 if %diff% lss %timeout% (
@@ -19,3 +16,14 @@ if %diff% lss %timeout% (
 ) else (
     cscript /nologo "%~dpn0.vbs" %1
 )
+exit/b
+
+:time2seconds
+set h=%TIME:~0,2%
+set m=%TIME:~3,2%
+set s=%TIME:~6,2%
+if %h:~0,1% equ 0 set h=%h:~1,1%
+if %m:~0,1% equ 0 set m=%m:~1,1%
+if %s:~0,1% equ 0 set s=%s:~1,1%
+set/a seconds=h*3600 + m*60 + s
+exit/b
