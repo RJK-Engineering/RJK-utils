8afaf058856a6b3527075191b01e65913bb58434
RJK-Engineering <rjklinkhamer@gmail.com>
2022-09-21 05:45:15 +0200

    suppress output

--- utils/system/sendkeys/sendkeys.bat
+++ utils/system/sendkeys/sendkeys.bat
@@ -9,7 +9,7 @@ timeout %timeout%
 :STOP
 :: this expression sometimes FAILS with "Invalid number."
 set/a stop=%TIME:~0,2%*3600 + %TIME:~3,2%*60 + %TIME:~6,2%
-if not defined stop timeout 1 & goto STOP
+if not defined stop timeout 1 >NUL & goto STOP
 
 set/a diff=stop-start
 if %diff% lss 0 set/a diff+=86400
